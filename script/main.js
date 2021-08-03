f595ad485210580a85fbd400892ac6ddf5715651
erickcapilla <41455478+erickcapilla@users.noreply.github.com>
2021-08-03 05:58:49 +0000

    Deploying to gh-pages from @ erickcapilla/vacaciones-cody@54401519acb7ddf0b9134571ffa3e0a3fd60fe10 🚀

---
+++ script/main.js
@@ -0,0 +1,23 @@
+// Functions
+function changeMode() {
+  document.body.style.transition = 'all 3s'
+
+  darkMode.checked ? document.documentElement.style.setProperty('--primary-color', '#050036')
+  : document.documentElement.style.setProperty('--primary-color', '#0092d1')
+}
+
+window.addEventListener('load', () => {
+  document.body.style.transition = ''
+  changeMode()
+})
+
+const darkMode = document.getElementById('mode_check');
+
+darkMode.addEventListener('click', () => {
+  const circle = document.querySelector('.mode-circle');
+
+  circle.style.transition = 'all 2s'
+  circle.classList.toggle('active')
+  changeMode()
+});
+
