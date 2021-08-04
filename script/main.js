117ccb3b6c035b911ef15f0922710f4a258f420a
erickcapilla <41455478+erickcapilla@users.noreply.github.com>
2021-08-04 05:00:55 +0000

    Deploying to gh-pages from @ erickcapilla/vacaciones-cody@9ed6f22f52d193228f7c83fd2de9fa47a444a1d6 🚀

--- script/main.js
+++ script/main.js
@@ -1,14 +1,31 @@
+const nameStorage = 'preferences';
+const darkMode_switch = document.getElementById('mode_check');
+const circle = document.querySelector('.mode-circle');
+
+darkMode_switch.addEventListener('click', () => {
+  circle.style.transition = 'all 2s'
+  changeMode()
+});
+
 // Functions
 function changeMode() {
-  document.body.style.transition = 'all 3s'
+  document.body.style.transition = 'all 3s';
   const sunMoon = document.getElementById('sunMoon');
 
-  if(darkMode.checked) {
-    document.documentElement.style.setProperty('--primary-color', '#050036')
-    sunMoon.style.backgroundImage = "url('https://img.icons8.com/office/96/000000/moon.png')"
+  if(darkMode_switch.checked) {
+
+    document.documentElement.style.setProperty('--primary-color', '#050036');
+    sunMoon.style.backgroundImage = "url('https://img.icons8.com/office/96/000000/moon.png')";
+    localStorage.setItem(nameStorage, JSON.stringify({darkMode: true}));
+    circle.classList.add('active');
+
   } else {
-    document.documentElement.style.setProperty('--primary-color', '#0092d1')
-    sunMoon.style.backgroundImage = "url('https://img.icons8.com/fluency/96/000000/sun.png')"
+
+    document.documentElement.style.setProperty('--primary-color', '#0092d1');
+    sunMoon.style.backgroundImage = "url('https://img.icons8.com/fluency/96/000000/sun.png')";
+    localStorage.setItem(nameStorage, JSON.stringify({darkMode: false}));
+    circle.classList.remove('active');
+
   }
 }
 
@@ -20,28 +37,22 @@ function scrollBar() {
   window.addEventListener('scroll', e => {
     let scrollValue = e.path[1].scrollY;
     let scrollHeight = document.documentElement.scrollHeight;
-    let scroll = ((scrollBar.clientHeight / scrollHeight) * 100)
-    let result = ((scroll * scrollValue) / 100) * 1.5;
+    let scroll = ((scrollBar.clientHeight / scrollHeight) * 100);
+    let result = ((scroll * scrollValue) / 100) * 1.2;
 
     sunMoon.style.top = String(Math.floor(result - 8)) + 'px';
   })
 }
 
-scrollBar()
 
 window.addEventListener('load', () => {
-  document.body.style.transition = ''
-  changeMode()
+  //LocalStorage
+  let preferences = localStorage.getItem(nameStorage);
+  preferences = JSON.parse(preferences);
+
+  (preferences.darkMode) ? darkMode_switch.checked = true
+  : darkMode_switch.checked = false;
+
+  changeMode();
+  scrollBar();
 })
-
-const darkMode = document.getElementById('mode_check');
-
-darkMode.addEventListener('click', () => {
-  const circle = document.querySelector('.mode-circle');
-
-  circle.style.transition = 'all 2s'
-  circle.classList.toggle('active')
-  changeMode()
-});
-
-
